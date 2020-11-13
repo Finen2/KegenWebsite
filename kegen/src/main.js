3a316a09cc30a3dde66ce04be5824ce741c29a3d
Tim Aro <timaro@live.se>
2020-11-13 17:38:35 +0100

    errors and warnings fixed

--- kegen/src/main.js
+++ kegen/src/main.js
@@ -1,14 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Vue from 'vue';
 import PrismicVue from '@prismicio/vue'
 import linkResolver from './link-resolver.js';
 
-// Add this before the new Vue instance
-Vue.use(PrismicVue, {
-  endpoint: "https://your-repo-name.cdn.prismic.io/api/v2",
+createApp(App)
+  .use(router)
+  .mount('#app')
+  .use(PrismicVue, {
+  endpoint: "https://kegenPortfolio.cdn.prismic.io/api/v2",
   linkResolver
 });
-
-createApp(App).use(router).mount('#app')
