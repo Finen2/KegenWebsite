7189492ea0800ad1110348551816d19c0e855695
Tim Aro <timaro@live.se>
2020-12-28 19:58:50 +0100

    addition of vue 2 version project

--- kegen/src/main.js
+++ kegen/src/main.js
@@ -1,19 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import PrismicVue from '@prismicio/vue'
-import linkResolver from './link-resolver.js';
+// import PrismicVue from '@prismicio/vue'
+// import linkResolver from './link-resolver.js';
 
 //Add this before the new Vue instance
-// app.use(PrismicVue, {
+// Vue.use(PrismicVue, {
 //   endpoint: "https://kegenPortfolio.cdn.prismic.io/api/v2",
 //   linkResolver
 // });
 
 createApp(App)
-  .use(PrismicVue, {
-    endpoint: "https://kegenportfolio.cdn.prismic.io/api/v2",
-    linkResolver
-  })
+  // .use(PrismicVue, {
+  //   endpoint: "https://kegenportfolio.cdn.prismic.io/api/v2",
+  //   linkResolver
+  // })
   .use(router)
   .mount('#app')
